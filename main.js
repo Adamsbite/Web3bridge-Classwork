e277456835305e3f7d7b83bc4c88acd0f9db5e9b
MR BITE <btadamz@gmail.com>
2023-08-05 17:24:57 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+import  * as ethers from 'ethers';
+ 
+const wallet = ethers. e(mnemonic);
+ 
+
+import fetch from 'node-fetch';
+
+ 
+  "jsonrpc": "2.0",
+  "id": 0,
+  "method": "alchemy_getAssetTransfers",
+  "params": [
+    {
+      "fromBlock": "0x0",
+      "fromAddress": "0x5c43B1eD97e52d009611D89b74fA829FE4ac56b1",
+    }
+  ]
+});
